4bf6b671d8bdf2588b920d966fa3773130504215
Pan <Ron.Pan@fisglobal.com>
2021-07-09 18:05:45 -0400

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+// Add the import
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+  // Call the loader
+defineCustomElements(window);
